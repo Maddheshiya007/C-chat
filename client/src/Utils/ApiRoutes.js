0125472d0d09150a14f2cce12a7cdcba44bd6c6d
Maddheshiya007 <chandanmaddhesiya4@gmail.ocm>
2024-02-24 21:52:10 +0530

    responsiveness added

--- client/src/Utils/ApiRoutes.js
+++ client/src/Utils/ApiRoutes.js
@@ -1,6 +1,7 @@
 
+// const host = "http://localhost:4000"; // for development
 
-const host = `${process.env.REACT_APP_BACKEND_URL}`;
+const host = `${process.env.REACT_APP_BACKEND_URL}`; // for production
 
 export const ApiRegister = `${host}/api/auth/register`;
 export const ApiLogin = `${host}/api/auth/login`;
